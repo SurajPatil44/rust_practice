8c1a71a6eeec65f91d294c008af255655bbb9cff
SurajPatil44 <sdpatil@qti.qualcomm.com>
2020-07-05 14:48:09 +0530

    concepts

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -5,10 +5,27 @@ Mutable : let mut a = 5 // a is mutable
 
 */
 
-fn another_function(){
-    println!("printing function");
+/*
+fn another_function(num : i32,num2 : i32){
+    let num = num % num2; //shadowing
+    println!("printing function {}",num);
+    
+    //num is freed when this returns 
+    //original num is intact in main 
+}
+*/
+
+fn liftoff(cntdn : i32){
+    for num in (1..cntdn).rev(){
+        println!("{}!",num);
+    }
+    println!("LIFTOFF!!!!!!!!!!");
 }
 
+
+fn squarer(num : i32) -> i32{
+    num * num
+}
 
 fn main() {
 
@@ -46,7 +63,31 @@ fn main() {
     let tup1 : (i32,f64,u8) = (500,6.8787,67);
     let (x,y,z) = tup1; 
     //let x = tup1.0;
+    
+    //uncomment fn another_function and run
+    let x = 5;
+    let y = 3;
+    another_function(x,y);
+    println!("x is {} ",x);
+    another_function(x,y);
+    
+    // using scopes to return value
+    
+    let x = 5;
+    // following is kinda inline 
+    let y = {
+        println!("provide a number for y");
+        let mut z = String::new();
+        std::io::stdin()
+            .read_line(&mut z)
+            .expect("failed to read");
+        let z: i32 = z.trim().parse().expect("not a number");
+        squarer(z)
+    
+    };
+    
+    println!("val of y {}",y);
     */
-    another_function();
+    liftoff(234);
     
 }
