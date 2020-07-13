2604a252f0c9ef544bf2d78d49b5f901410109c9
SurajPatil44 <sdpatil@qti.qualcomm.com>
2020-07-13 17:20:12 +0530

    mygrep : made it faster using closures \n added tests

--- mygrep/src/lib.rs
+++ mygrep/src/lib.rs
@@ -3,20 +3,49 @@ use std::error::Error;
 
 pub struct Config{
     fname : String,
-    pattern : String
+    pattern : String,
+    case_insensitive : bool
 }
 
+#[derive(Debug,Eq,PartialEq)]
 pub struct SearchInfo{
     lineno : u32,
     line : String
 }
 
 impl Config{
-    pub fn new(args: &[String]) -> Result<Self,&'static str>{
+    pub fn new(mut args: std::env::Args) -> Result<Self,&'static str>{
+        args.next(); //filename
+        /*
         let file = args[1].clone();
         let pattern = args[2].clone();
+        */
+        let file = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Filename is not provided")
+        };
+        let pattern = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Query pattern is not provided")
+        };
         
-        Ok(Config{fname:file,pattern:pattern})
+        
+        let case_insensitive = match args.next(){
+            Some(arg) => {
+                let arg = String::from(arg);
+                if arg == "0"{
+                    false
+                }else{
+                    true
+                }
+            },
+            None => true
+        };
+        
+        Ok(Config{fname:file,
+               pattern,
+               case_insensitive
+        })
     }
 }
 
@@ -24,7 +53,7 @@ pub fn run(config : Config) -> Result<(),Box<dyn Error>>{
     
     let contents = fs::read_to_string(config.fname)?;
     //&contents
-    for si in search(&contents,&config.pattern,true){
+    for si in search(&contents,&config.pattern,config.case_insensitive){
         eprintln!("{}. {}",si.lineno,si.line);
     }
     Ok(())
@@ -33,37 +62,30 @@ pub fn run(config : Config) -> Result<(),Box<dyn Error>>{
 pub fn search<'a> (content : &'a str,query : &str,
                     case_insensitive : bool) -> Vec<SearchInfo>
 {
+    
     let mut results = Vec::new();
+    let cont_iter = content
+                    .lines()
+                    .enumerate();
     match case_insensitive{
         true => {
                     let query = query.to_lowercase();
-    
-                    for (index,line) in content.lines().enumerate(){
-                        if line.to_lowercase().contains(&query){
-                            let si = SearchInfo{
-                                        lineno : index as u32,
-                                        line : line.to_string()
-                                    };
-                            results.push(si);
-                        }
-                    } 
+                    results = cont_iter
+                                .map(|(i,line)| SearchInfo{lineno: (i + 1) as u32,line:line.to_string()})
+                                .filter( |si| si.line.to_lowercase().contains(&query))
+                                .collect();              
+                    
                 },
-        false =>{
-                    for (index,line) in content.lines().enumerate(){
-                        if line.contains(query){
-                            let si = SearchInfo{
-                                        lineno : index as u32,
-                                        line : line.to_string()
-                                    };
-                            results.push(si);
-                        }
-                    } 
+        false =>{ 
+                    results = cont_iter
+                                .map(|(i,line)| SearchInfo{lineno: (i + 1) as u32,line:line.to_string()})
+                                .filter( |si| si.line.contains(&query))
+                                .collect(); 
                 }
     }
     results
             
 } 
-
 
 
 #[cfg(test)]
@@ -79,8 +101,21 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
-        assert_eq!(vec!["safe, fast, productive."], 
-                    search(contents,query,false));
+        let answer = search(contents,query,false);
+        /*
+        for ans in answer{
+            println!("{} {}",ans.lineno,ans.line);
+        }
+        */
+        assert_eq!(vec![
+                       SearchInfo{
+                        lineno : 2,
+                        line: String::from("safe, fast, productive."),
+                       }
+                    ], 
+                    answer
+                   );
+        
     }
 
     #[test]
@@ -91,10 +126,24 @@ Rust:
 safe, fast, productive.
 Pick three.
 Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search(contents,query,true)
-        );
+        let answer = search(contents,query,true);
+        /*
+        for ans in answer{
+            println!("{} {}",ans.lineno,ans.line);
+        }
+        */
+        assert_eq!(vec![
+                       SearchInfo{
+                        lineno : 1,
+                        line: String::from("Rust:"),
+                       },
+                       SearchInfo{
+                        lineno : 4,
+                        line: String::from("Trust me."),
+                       },
+                    ], 
+                    answer
+                   );
+        
     }
 }
